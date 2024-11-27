84afdf1ed10f01e7b252f38b51eaa30388e7a3ca
Chloé Cilia <chloe.cilia68@gmail.com>
2024-11-27 17:02:59 +0100

    main push

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ajout produit</title>
+        <!-- FEUILLES DE STYLE -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.21.16/dist/css/uikit.min.css" />
+        <link rel="stylesheet" href="styles_index.css">
+        <!-- FONTS -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.21.16/js/uikit.min.js" integrity="sha512-vKDeN+i81+kMaXLe0V9cAAFaYRsVbX61IV4B6JYZIGa8rM9cNlRrMqiKKe358+A3tpO9W8yjaxC3O/AVij3sog==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    </head>
+
+    <body>
+    <nav class="uk-navbar-container">
+        <div class="uk-container">
+            <div uk-navbar>
+                <div class="uk-navbar-left">
+                    <ul class="uk-navbar-nav">
+                        <li class="uk-active"><a href="#">Retour page précédente</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+        <div id="mainContent__wrapper">
+            <h1>Ajouter un produit</h1>
+                <div id="form__wrapper">
+                    <form action="traitement.php" method="post">
+                        <p>
+                            <label>
+                                Nom du produit : 
+                                <input class="champsFormulaire" type="text" name="name">
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Prix du produit : 
+                                <input class="champsFormulaire" type="number" step="any" name="price">
+                            </label>
+                        </p>
+                        <p>
+                            <label>
+                                Quantité désirée :
+                                <input class="champsFormulaire" type="number" name="qtt" value="1">
+                            </label>
+                        </p>
+                        <p>
+                            <input id="boutonAjouter" type="submit" name="submit" value="Ajouter le produit">
+                        </p>
+                    </form>
+                </div> 
+        </div>   
+
+ 
+        <div class="darksoul-footer">
+            <div class="darksoul-footer-top">
+            </div>
+                <div class="darksoul-footer-section">
+                    <div class="logo-head">
+                        <b>Follow us on</b>
+                    </div>
+                    <div class="logo">
+                    <a href="#" target="_blank"><img width="30" height="30" src="https://img.icons8.com/ios-glyphs/100/instagram-new.png" alt="instagram-new"/></a>
+                    </div>    
+                </div>
+            </div>
+            <div class="darksoul-footer-bottom">
+                <div class="darksoul-footer-bottom-sec">
+                    <p>Privacy</p>
+                </div>
+                <div class="darksoul-footer-bottom-sec">
+                    <p>&copy; 2024</p>
+                </div>
+                <div class="darksoul-footer-bottom-sec">
+                    <p>Contact us</p>
+                </div>
+            </div> 
+        </div>
+       
+    </body>
+</html>
+
+
+
+<!-- DEFINITION D'UNE SESSION : La session est un moyen permettant de stocker des données individuelles de chaque utilisateur, en utilisant
+un identifiant de session unique.
+Les identifiants de session sont envoyés au navigateur via des cookies de session.
+L'identifiant est utilisé pour récupérer les données existantes de la session. 
+En l'absence d'un cookie de session ou d'un identifiant de session, PHP crée une nouvelle session et génère un nouvel id. 
+=> au démarrage d'une session, PHP récupère une session existante  ou en crée une nouvelle 
+Note : une session peut être démarrée manuellement avec la fonction session_start()
+les session s'arrêtent automatiquement à la fermeture du navigateur ou peuvent être stoppées manuellement avec session_destroy();
+
+DEFINITION D'UNE SUPERGLOBALE : les superglobales sont des variables prédéfinies en PHP, disponibles quel que soit le contexte du script.
+Les 9 variables superglobales sont :
+    $GLOBALS
+    $_SERVER
+    $_GET
+    $_POST
+    $_FILES
+    $_COOKIE
+    $_SESSION
+    $_REQUEST
+    $_ENV
+
+DEFINITION D'UNE FAILLE XSS : Cross-site scripting est une faille de sécurité par laquelle est injectée du script via les paramètres
+d'entrée côté client (dans un champ de formulaire ou dans l'adresse URL).
+Il existe trois types d'attaques XSS :
+- les attaques stockées (stored XSS) : le script est stocké sur le serveur 
+- les attaques reflettées (reflected XSS) : le script est renvoyé dans la réponse du serveur car il n'est pas stocké
+- les attaques basées sur le DOM (DOM-based XSS) : le script n'utilise pas le serveur et permet la modification de la structure DOM
+Une des manières sûre de parer les attaques XSS : l'echappement -> tous les caractères spéciaux sont remplacés par des valeurs encodés, de manière
+à ce que toute donnée saisie par un utilisateur soit traitée comme du texte. -->
+
+<!-- + filter_input, filter_var, htmlentities, htmlspecialchars -->
---
+++ traitement.php
@@ -0,0 +1,58 @@
+<?php
+    session_start();
+
+    if(isset($_POST['submit'])){
+        $name = filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING);
+        $price = filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $qtt = filter_input(INPUT_POST,"qtt",FILTER_VALIDATE_INT);
+
+        if($name && $price && $qtt){
+
+            $product = [
+                "name" => $name,
+                "price" => $price,
+                "qtt" => $qtt,
+                "total" => $price*$qtt
+            ];
+
+            $_SESSION['products'][]=$product;
+
+        }
+    }
+
+    header("Location:index.php");
+
+
+// => session_start(); -> cette fonction permet de démarrer une session de l'utilisateur sur le serveur OU récupérer la session de cet
+//utilisateur. La récupération est rendue possible par l'enregistrement par le serveur d'un COOKIE PHPSESSID dans le navigateur client. 
+//Les cookies sont transmis au serveur par chaque requête HTTP effectuée par le client. 
+//La durée de vie d'un cookie dépend de la configuration serveur. Par défaut, le cookie expire à la fermeture du navigateur :
+//Expiration/Max-Age=Session
+
+// => if(isset($_POST['submit'])){} -> permet de vérifier l'existence de la clé "submit" dans le tableau $_POST
+//La clé 'submit' correspond à l'attribut 'name' du bouton <input type="submit" name ="submit">.
+
+// => header() -> dans le cas où la condition précédente est fausse, cette fonction effectue une redirection en envoyant un nouvel entête HTTP
+// au client. 
+// /!\ la fonction header() nécessite 2 précautions :
+// 1. pas d'émission d'un début de réponse avant header() par la page 
+// 2. header() doit être la dernière instruction du fichier ou être immédiatement suivie de exit() ou die(), en raison de l'exécution du script
+//courant.
+
+// => FILTER_SANITIZE_STRING (champ "name") -> ce filtre supprime une chaîne de caractères de toute présence de caractères spéciaux
+// et de toute balise HTML potentielle ou les encode -> impossible d'injecter du HTML
+
+// => FILTER_VALIDATE_FLOAT (champ "price") : validera le prix que s'il est un nombre à virgule (pas de texte ou autre…)
+// le drapeau FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale.
+
+// => filter_input() renvoie en cas de succès la valeur assainie correspondant au champ traité,
+// false si le filtre échoue ou null si le champ sollicité par le nettoyage n'existait pas dans la requête POST.
+
+// => if($name && $price && $qtt){} -> permet de vérifier si les filtres ont fonctionné. La condition = la variale contient-elle une valeur
+//positive (string, number, ...) ou false, null, 0?
+
+// le tableau associatif $product et $SESSION['products'][]=$product; => permet de stocker les données en session,
+// en l'ajoutant au tableau $_SESSION
+
+//['product'] => permet de créer la clé (si 1er article ajouté au panier) ou de récupérer l'existante
+// [] => raccourci indiquant que nous ajoutons une nouvelle entrée au futur tableau "productS" associé à cette clé
